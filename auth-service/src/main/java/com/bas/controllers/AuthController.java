9a0aa7580405a09ced1240e4288c3b2b7f50a500
Kostiantyn Pavlunenko <kostiantyn.pavlunenko@oril.co>
2023-06-30 15:34:02 +0300

    Refactor Auth service

--- auth-service/src/main/java/com/bas/controllers/AuthController.java
+++ auth-service/src/main/java/com/bas/controllers/AuthController.java
@@ -3,7 +3,7 @@ package com.bas.controllers;
 import com.bas.models.dto.AuthRequest;
 import com.bas.models.dto.AuthResponse;
 import com.bas.services.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,16 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping(value = "/auth")
-//@AllArgsConstructor
 public class AuthController {
 
 	private final AuthService authService;
-
-	@Autowired
-	public AuthController(final AuthService authService) {
-		this.authService = authService;
-	}
 
 	@PostMapping(value = "/register")
 	public ResponseEntity<AuthResponse> register(@RequestBody AuthRequest authRequest) {
--- auth-service/src/main/java/com/bas/services/AuthService.java
+++ auth-service/src/main/java/com/bas/services/AuthService.java
@@ -2,31 +2,24 @@ package com.bas.services;
 
 import com.bas.models.dto.AuthRequest;
 import com.bas.models.dto.AuthResponse;
-import com.bas.models.entities.UserVO;
+import com.bas.models.entities.User;
+import lombok.AllArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-//@AllArgsConstructor
+@AllArgsConstructor
 public class AuthService {
 
 	private final RestTemplate restTemplate;
 	private final JwtUtil jwtUtil;
 
-	@Autowired
-	public AuthService(RestTemplate restTemplate,
-	                   final JwtUtil jwt) {
-		this.restTemplate = restTemplate;
-		this.jwtUtil = jwt;
-	}
-
 	public AuthResponse register(AuthRequest authRequest) {
 		//TODO: add validation if user exist in db
 		authRequest.setPassword(BCrypt.hashpw(authRequest.getPassword(), BCrypt.gensalt()));
 
-		UserVO registeredUser = restTemplate.postForObject("http://user-service/users", authRequest, UserVO.class);
+		User registeredUser = restTemplate.postForObject("http://user-service/users", authRequest, User.class);
 
 		String accessToken = jwtUtil.generate(registeredUser.getId().toString(), registeredUser.getRole(), "ACCESS");
 		String refreshToken = jwtUtil.generate(registeredUser.getId().toString(), registeredUser.getRole(), "REFRESH");
