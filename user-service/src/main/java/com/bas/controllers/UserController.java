e884aa76c8e323661c69b7437c8183f0496e238b
Kostiantyn Pavlunenko <kostiantyn.pavlunenko@oril.co>
2023-06-30 15:54:47 +0300

    Refactor User service

--- user-service/src/main/java/com/bas/controllers/UserController.java
+++ user-service/src/main/java/com/bas/controllers/UserController.java
@@ -2,7 +2,7 @@ package com.bas.controllers;
 
 import com.bas.models.database.User;
 import com.bas.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
-//@AllArgsConstructor
+@AllArgsConstructor
 public class UserController {
 
-	@Autowired
-	private UserService userService;
+	private final UserService userService;
 
 	@PostMapping
 	public User save(@RequestBody User user) {
--- user-service/src/main/java/com/bas/services/UserService.java
+++ user-service/src/main/java/com/bas/services/UserService.java
@@ -2,19 +2,14 @@ package com.bas.services;
 
 import com.bas.models.database.User;
 import com.bas.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-//@AllArgsConstructor
+@AllArgsConstructor
 public class UserService {
 
 	private final UserRepository userRepository;
-
-	@Autowired
-	public UserService(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
 
 	public User save(User user) {
 		return userRepository.save(user);
